3db3e2887a480a8f8b58fcb1d14aa149ee1e896d
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-06-20 17:57:04 -0700

    top nav placed

---
+++ src/components/TopNav/TopNav.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+}
+
+class TopNav extends Component {
+    render() {
+        const {classes} = this.props
+
+        return (
+            <div className={{ root: styles.root }}>
+                <AppBar position="static" color="default">
+                    <Toolbar>
+                        <Typography variant="h6" color="inherit">
+                            Clicky Game
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+    }
+}
+
+export default TopNav
