ac4bcf1bb2f11cbcda325e2d66c6ebc82b962013
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-06-25 10:55:35 -0700

    clear tree

--- src/components/ImgButton/ImgButton.js
+++ src/components/ImgButton/ImgButton.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react'
 import './ImgButton.css'
 import Button from '@material-ui/core/button'
+// npm package to create shake effect
 
 class ImgBtn extends Component {
     render() {
         const { imgArr, handleClickImage } = this.props
+        
 
         return (
-            <div id='container' style={{ margin: '50px 250px' }}> {
-                imgArr.map(img => {
-                    return <Button className='imgBtn' style={{ height: '120px', width: '160px', marginBottom: '20px' }} onClick={() => handleClickImage(img)}>
-                        <img className='image' style={{ maxWidth: '100%', maxHeight: '100%' }} src={img} alt='landmark'></img>
-                    </Button>
-                })
-            }
-            </div>
+                <div id='container' style={{ margin: '50px 250px' }}> {
+                    imgArr.map(img => {
+                        return <Button className='imgBtn' style={{ height: '120px', width: '160px', marginBottom: '20px' }} onClick={() => handleClickImage(img)}>
+                            <img className='image' style={{ maxWidth: '100%', maxHeight: '100%' }} src={img} alt='landmark'></img>
+                        </Button>
+                    })
+                }
+                </div>
         )
     }
 }
