b71e5bc22dc6d0cc89e39843680ccbf3a11300d7
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-06-25 11:11:23 -0700

    Updates

---
+++ precache-manifest.97319c088ce5394b341b12805a24c4cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8bd960b46c9765cd9e56dd8e5f77170",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "27d8f20e6fd11062e3ed",
+    "url": "/ClickyGame/static/css/main.d89d22e4.chunk.css"
+  },
+  {
+    "revision": "9d50f3fcfe4d41d94d40",
+    "url": "/ClickyGame/static/js/2.ae6fbdd5.chunk.js"
+  },
+  {
+    "revision": "27d8f20e6fd11062e3ed",
+    "url": "/ClickyGame/static/js/main.17f706ef.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "a06a3248f173f51bcbbcdd434a1de129",
+    "url": "/ClickyGame/static/media/white-waves.a06a3248.png"
+  }
+]);
