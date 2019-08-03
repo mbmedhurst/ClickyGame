7d354cf479f474b1ef99887825f15d7095ff0a09
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-08-02 20:46:48 -0700

    contain image layout

--- src/components/ImgButton/ImgButton.js
+++ src/components/ImgButton/ImgButton.js
@@ -12,7 +12,7 @@ class ImgBtn extends Component {
             <Grid container className='pageCont'>
             <Grid container className='imgCont' style={{ marginTop: '100px', marginLeft: '15%', marginRight: '15%'}}> {
                 imgArr.map(img => {
-                    return <Grid item sm={3} md={3}>
+                    return <Grid item sm={3} md={3} lg={3} xl={3}>
                         <Button className='imgBtn' style={{ height: '120px', width: '160px', marginBottom: '20px' }} onClick={() => handleClickImage(img)}>
                             <img className='image' style={{ maxWidth: '110%', maxHeight: '110%' }} src={img} alt='landmark'></img>
                         </Button>
